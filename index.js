e1beccfd9cf6e554733b50a50c213822a23a3510
Adam <adam.crespo101@outlook.com>
2022-03-02 11:48:08 -0500

    commit tree 4869764d246770c9263f430f8b42b84af9023407

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((books) => books.json())
+  .then((books) => renderBooks(books));
+  }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
